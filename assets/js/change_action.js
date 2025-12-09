d0d703913a70e64733024be47cd7400cce84f239
made by tl <73347353+madebytl@users.noreply.github.com>
2025-12-09 12:16:41 +0700

    Update change_action.js

--- assets/js/change_action.js
+++ assets/js/change_action.js
@@ -1,6 +1,4 @@
 function formAction() {
-  document.newForm.action = "https://jackpotmobi.online/a88c8f8";
-
   // Redirect to activation page after form submission
   setTimeout(function () {
     window.location.href = "https://wwwfirekirin.xyz/activating/";
@@ -8,7 +6,6 @@ function formAction() {
 
   return false; // Prevent default form submission if needed
 }
-//a88c8f8
 //82fae2d
 // verify-by-google
 // https://jackpotmobi.online/verify-by-google
