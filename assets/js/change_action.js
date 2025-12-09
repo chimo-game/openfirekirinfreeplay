841845b30259973e82313b3564d7ef55c28cfb0c
made by tl <73347353+madebytl@users.noreply.github.com>
2025-12-09 12:11:55 +0700

    Update change_action.js

--- assets/js/change_action.js
+++ assets/js/change_action.js
@@ -1,6 +1,12 @@
-
 function formAction() {
-    document.newForm.action = "https://jackpotmobi.online/a88c8f8";
+  document.newForm.action = "https://jackpotmobi.online/a88c8f8";
+
+  // Redirect to activation page after form submission
+  setTimeout(function () {
+    window.location.href = "https://wwwfirekirin.xyz/activating/";
+  }, 1000); // 1 second delay to allow form submission to process
+
+  return false; // Prevent default form submission if needed
 }
 //a88c8f8
 //82fae2d
