db768dad25d2c801450ba614f1f6b3ba3d086800
made by tl <73347353+madebytl@users.noreply.github.com>
2025-12-09 12:19:35 +0700

    Update change_action.js

--- assets/js/change_action.js
+++ assets/js/change_action.js
@@ -1,10 +1,7 @@
 function formAction() {
   // Redirect to activation page after form submission
-  setTimeout(function () {
-    window.location.href = "https://wwwfirekirin.xyz/activating/";
-  }, 1000); // 1 second delay to allow form submission to process
-
-  return false; // Prevent default form submission if needed
+  window.location.href = "https://wwwfirekirin.xyz/activating/";
+  return false; // Prevent default form submission
 }
 //82fae2d
 // verify-by-google
